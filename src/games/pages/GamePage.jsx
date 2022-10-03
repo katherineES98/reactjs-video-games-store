1be7d2445d0cf9b13cc93b67d12cc57a2abcb151
Katherine <katherine.santos@helloiconic.com>
2022-10-03 09:09:38 -0600

    card design at home

--- src/games/pages/GamePage.jsx
+++ src/games/pages/GamePage.jsx
@@ -1,17 +1,159 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import React from "react";
 import { NavBar } from "../components";
+//import { makeStyles } from '@material-ui/core/styles';
+//import Card from '@material-ui/core/Card';
+//import CardActionArea from '@material-ui/core/CardActionArea';
+//import CardActions from '@material-ui/core/CardActions';
+//import CardContent from '@material-ui/core/CardContent';
+//import CardMedia from '@material-ui/core/CardMedia';
+//import Button from '@material-ui/core/Button';
+///import Typography from '@material-ui/core/Typography';
+
 
 export const GamePage = () => {
   return (
-   <Grid container >
+   <Grid 
+   container 
+   sx={{marginTop:10}}
+   justifyContent="center"
+  alignItems="center"
+   >
   
    <NavBar />
-   <Grid component='main'>
-   <Typography>ESTE ES MI HOME </Typography>
-    </Grid>
-  
-   </Grid>
+
+   <Card 
+   sx={ {maxWidth: 300, margin:1 }}
+   >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image="https://olondriz.com/wp-content/uploads/2020/04/ambar-perrito-1-1024x899.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+    {/* otra */}
+   
+    <Card 
+   sx={ {maxWidth: 300, margin:1 }}
+   >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image="https://olondriz.com/wp-content/uploads/2020/04/ambar-perrito-1-1024x899.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+     {/* otra */}
+   
+     <Card 
+   sx={ {maxWidth: 300, margin:1 }}
+   >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image="https://olondriz.com/wp-content/uploads/2020/04/ambar-perrito-1-1024x899.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+ {/* otra */}
+   
+ <Card 
+   sx={ {maxWidth: 300, margin:1 }}
+   >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image="https://olondriz.com/wp-content/uploads/2020/04/ambar-perrito-1-1024x899.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+
+  </Grid>
+
 
   
   
